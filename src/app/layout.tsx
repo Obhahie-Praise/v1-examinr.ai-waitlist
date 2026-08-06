2f727a202b1abfebf20fe0cd59bcac363d6ec13c
Obhahie-Praise <obhahiepraise@gmail.com>
2026-08-06 20:30:32 +0100

    feat: implement root layout with custom fonts and Vercel analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
-import { Commissioner, Instrument_Serif, Instrument_Sans } from "next/font/google";
+import {
+  Commissioner,
+  Instrument_Serif,
+  Instrument_Sans,
+} from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const commissioner = Commissioner({
@@ -23,23 +28,23 @@ export const metadata: Metadata = {
   title: "Examinr.ai Waitlist",
   description: "The official public waitlist for Examinr.ai",
   icons: {
-    icon: "/favicon.svg"
+    icon: "/favicon.svg",
   },
   openGraph: {
-  title: "Examinr.ai waitlist",
-  description: "The official public waitlist for Examinr.ai",
-  url: "https://examinr-ai.vercel.app",
-  siteName: "Examinr.ai",
-  type: "website",
-  images: [
-    {
-      url: "https://examinr-ai.vercel.app/hero.png",
-      width: 1200,
-      height: 630,
-      alt: "Examinr.ai",
-    },
-  ],
-}
+    title: "Examinr.ai waitlist",
+    description: "The official public waitlist for Examinr.ai",
+    url: "https://examinr-ai.vercel.app",
+    siteName: "Examinr.ai",
+    type: "website",
+    images: [
+      {
+        url: "https://examinr-ai.vercel.app/hero.png",
+        width: 1200,
+        height: 630,
+        alt: "Examinr.ai",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -52,7 +57,10 @@ export default function RootLayout({
       lang="en"
       className={`${commissioner.variable} ${instrumentSerif.variable} ${instrumentSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-primary text-text-accent bg-app-bg">{children}</body>
+      <Analytics />
+      <body className="min-h-full flex flex-col font-primary text-text-accent bg-app-bg">
+        {children}
+      </body>
     </html>
   );
 }
